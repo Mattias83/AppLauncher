b15cb9ebf1d4ef646105d004f28be90a79868ecd
Mattias83 <qvintus@gmail.com>
2024-11-19 15:21:16 +0100

    added execute method to App superclass

--- app_launcher/app_parsers/app.py
+++ app_launcher/app_parsers/app.py
@@ -1,4 +1,5 @@
 import gi
+from subprocess import Popen
 
 gi.require_version("Gtk", "4.0")
 from gi.repository import GObject
@@ -14,6 +15,15 @@ class App(GObject.GObject):
         self.app_type = app_type
         self.app_provider = app_provider
         self.icon = icon
+
+    def execute(self) -> None:
+        app_provider = self.app_provider
+        if app_provider == "Gnome":
+            Popen(self.exec.split(" "))
+        elif app_provider == "Flatpak":
+            Popen(["flatpak", "run", self.exec])
+        elif app_provider == "AppLauncher":
+            Popen([self.exec])
 
     def __str__(self) -> str:
         return f"Name: {self.name}, Exec: {self.exec}, AppType: {self.app_type}, AppProvider: {self.app_provider}, Icon: {self.icon}"
