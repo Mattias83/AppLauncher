0db75cf5929ad7d5885456ba1b4ed9365a304efc
Mattias83 <qvintus@gmail.com>
2024-11-19 09:41:35 +0100

    added css support for styling ui

--- app_launcher/application.py
+++ app_launcher/application.py
@@ -12,6 +12,16 @@ class Application(Gtk.Application):
             application_id="se.mattias83.applauncher",
             **kwargs,
         )
+        self.load_css()
+
+    def load_css(self) -> None:
+        css_provider = Gtk.CssProvider()
+        css_provider.load_from_path("styles.css")
+        Gtk.StyleContext.add_provider_for_display(
+            Gdk.Display.get_default(),
+            css_provider,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION,
+        )
 
     def do_activate(self) -> None:
         self.window = MainWindow(self)
