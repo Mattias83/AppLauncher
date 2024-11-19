5bf128097397207dccddb6c2a06c6d0cc902ba81
Mattias83 <qvintus@gmail.com>
2024-11-19 09:53:14 +0100

    comment with example for AppLauncher

--- app_launcher/app_parsers/app_launcher.py
+++ app_launcher/app_parsers/app_launcher.py
@@ -4,6 +4,15 @@ from app_launcher.app_parsers.app import App
 
 
 class AppLauncherApp(App):
+
+    # Create custom app shortcuts using json. Example:
+    # {
+    #     "name": "Visual Studio Code",
+    #     "exec": "/home/username/Code/utilities/ide/vscode/VSCode-linux-x64/code",
+    #     "type": "desktop",
+    #     "icon": "/home/username/Code/utilities/ide/vscode/vs-code-icon.png"
+    # }
+
     def __init__(self, path: str) -> None:
         app = json.load(open(path, "r"))
         super().__init__(
