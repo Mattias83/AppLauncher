95410ce84ba9450b1b24c23f0fa0f4566c92263c
Mattias83 <qvintus@gmail.com>
2024-11-19 09:45:45 +0100

    added simple App superclass

---
+++ app_launcher/app_parsers/app.py
@@ -0,0 +1,12 @@
+class App:
+    def __init__(
+        self, name: str, exec: str, app_type: str, app_provider: str, icon: str = ""
+    ) -> None:
+        self.name = name
+        self.exec = exec
+        self.app_type = app_type
+        self.app_provider = app_provider
+        self.icon = icon
+
+    def __str__(self) -> str:
+        return f"Name: {self.name}, Exec: {self.exec}, AppType: {self.app_type}, AppProvider: {self.app_provider}, Icon: {self.icon}"
